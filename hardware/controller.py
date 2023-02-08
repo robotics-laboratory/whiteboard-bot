56021905c4092c4883a03e465c836ea5448d6525
enaix <enaix@protonmail.com>
2023-02-08 20:32:13 +0300

    Add controller firmware

---
+++ hardware/controller.py
@@ -0,0 +1,68 @@
+import websockets
+import asyncio
+from pynput import keyboard
+import sys
+
+class Controller:
+    def __init__(self):
+        self.direction = 0
+        self.increment = 10
+        self.last_state = "0"
+
+    def rotate(self, d):
+        if d == 1 and self.direction < 90:
+            self.direction = min(self.direction + self.increment, 90)
+        elif d == -1 and self.direction > -90:
+            self.direction = max(self.direction - self.increment, -90)
+
+    async def init_ip(self, ip):
+        print("Connecting...")
+        self.ws = await websockets.connect("wss://"+ip+"/ws", port=80, ssl=False)
+        print("Connected")
+
+    async def send(self, msg):
+        if msg == "left":
+            self.rotate(-1)
+            msg = self.last_state
+        elif msg == "right":
+            self.rotate(1)
+            msg = self.last_state
+
+        self.last_state = msg
+        await self.ws.send(msg + ";" + str(self.direction + 90) + ";")
+
+ctr = Controller()
+
+keys_bind = {"Key.left": "left", "Key.right": "right", "Key.up": "0", "Key.down": "1"}
+
+def init_listener():
+    queue = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+
+    def press_event(key):
+        if str(key) in list(keys_bind):
+            loop.call_soon_threadsafe(queue.put_nowait, keys_bind[str(key)])
+
+    def release_event(key):
+        loop.call_soon_threadsafe(queue.put_nowait, "2")
+
+    listener = keyboard.Listener(
+        on_press=press_event,
+        on_release=release_event)
+    
+    listener.start()
+
+    return queue
+
+async def main():
+    ip = sys.argv[1]
+    await ctr.init_ip(ip)
+
+    queue = init_listener()
+    while True:
+        msg = await queue.get()
+        await ctr.send(msg)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
---
+++ hardware/wbb/wbb.ino
@@ -0,0 +1,194 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#define IN1 33 // 1
+#define IN2 32 // 1
+#define IN3 26 // 2
+#define IN4 25 // 2
+#define ENA 27 // speed A
+#define ENB 14 // speed B
+
+#define MAX_SPEED_A 255
+#define MAX_SPEED_B 255
+
+const char* ssid = "";
+const char* password = "";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+bool fwd1 = true;
+bool fwd2 = true;
+bool fwd3 = true;
+bool fwd4 = true;
+int speed = 0;
+
+int rotation = 0; // -90 - 90
+
+enum Direction
+{
+ 	FWD = 0,
+	BACK = 1,
+	STOP = 2,
+};
+
+Direction dir = STOP, cur_dir = STOP;
+
+void go(bool dir1, bool dir2, bool dir3, bool dir4)
+{
+	fwd1 = dir1;
+	fwd2 = dir2;
+	fwd3 = dir3;
+	fwd4 = dir4;
+}
+
+void switchState()
+{
+	switch(dir)
+	{
+		case FWD:
+			go(false, true, false, true);
+			break;
+		case BACK:
+			go(true, false, true, false);
+			break;
+		case STOP:
+			go(true, true, true, true);
+			break;
+	}
+}
+
+void smoothMovement()
+{
+	if (cur_dir == dir)
+	{
+		if (dir == STOP)
+			return; // set speed to 0
+		
+		if (speed < 255)
+			speed++;
+		return;
+	}
+
+	if (speed == 0)
+	{
+		cur_dir = dir;
+		switchState();
+		return;
+	}
+
+	speed--;
+}
+
+bool begins(char* str, char pattern[], size_t str_len)
+{
+	size_t p_size = sizeof(pattern)/sizeof(char);
+	if (str_len / sizeof(char) < p_size)
+		return false;
+	for (size_t i = 0; i < p_size; i++)
+	{
+		if (str[i] != pattern[i])
+			return false;
+	}
+	return true;
+}
+
+void event(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+	if (type == WS_EVT_DATA)
+	{
+		AwsFrameInfo *info = (AwsFrameInfo*)arg;
+		if (info->final && info->index == 0 && info->len == len)
+		{
+			data[len] = 0;
+			if (info->opcode == WS_TEXT)
+			{
+				char* state = (char*)data;
+				
+				char* tok = strtok(state, ";");
+				if (tok == NULL)
+					return;
+				int code = tok[0] - '0';
+
+				if (code >= 0 && code <= 2)
+				{
+					dir = static_cast<Direction>(code);
+				}
+				else
+					return;
+
+				tok = strtok(NULL, ";");
+				if (tok == NULL)
+					return;
+				
+				rotation = atoi(tok) - 90;
+			}
+		}
+	}
+}
+
+int mapMaxSpeed(int speed, int max)
+{
+	return map(speed, 0, 255, 0, max);
+}
+
+float calcDirection(size_t motor)
+{
+	if (motor == 0)
+		return float(map(rotation, -90, 90, 0, 100))/100;
+	else
+		return float(map(rotation, -90, 90, 100, 0))/100;
+}
+
+void spinMotors()
+{
+	digitalWrite(IN1, fwd1);
+	digitalWrite(IN2, fwd2);
+	digitalWrite(IN3, fwd3);
+	digitalWrite(IN4, fwd4);
+	analogWrite(ENA, mapMaxSpeed(speed * calcDirection(0), MAX_SPEED_A));
+	analogWrite(ENB, mapMaxSpeed(speed * calcDirection(1), MAX_SPEED_B));
+}
+
+void setup()
+{
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(IN3, OUTPUT);
+	pinMode(IN4, OUTPUT);
+	pinMode(ENA, OUTPUT);
+	pinMode(ENB, OUTPUT);
+
+	// motors check
+	speed = 100;
+	go(false, true, false, true);
+	spinMotors();
+	delay(300);
+	go(true, true, true, true);
+	spinMotors();
+	speed = 0;
+
+	Serial.begin(9600);
+
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(1000);
+	}
+	Serial.println("Wifi connected");
+
+	ws.onEvent(event);
+	server.addHandler(&ws);
+
+	server.begin();
+}
+
+void loop()
+{
+	smoothMovement();
+
+	spinMotors();
+
+	ws.cleanupClients();
+}
