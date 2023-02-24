c8c0a70a97d062fbfc48117032f3c6d81e054056
Gordey <105369166+Asm0deusss@users.noreply.github.com>
2023-02-24 23:32:41 +0300

    Camera calibration (#1)
    * Camera calibration
    
    * Camera calibration
    
    * Refactor old code, add logger
    
    * Add ROS logger
    
    * Refactor calibration code, add two topics
    
    * Clean up
    
    * Add frame size, clean up
    
    * Fixed a typo
    
    * Rename functions, fix typos, clean code, add .gitignore
    
    * small fixes
    
    ---------
    
    Co-authored-by: Simagin Denis <simagin.mail@yandex.ru>

---
+++ packages/camera/include/camera/camera_node.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "camera/params.h"
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/aruco.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+
+#include <memory>
+
+namespace wbb {
+
+class CameraNode : public rclcpp::Node {
+  public:
+    CameraNode();
+
+  private:
+    void handleCameraOnTimer();
+    cv::Mat getImage();
+    cv::Mat removeDistortion(const cv::Mat& raw_image);
+    void tryUpdateHomography(const cv::Mat& undistorted_image);
+    cv::Mat warp(const cv::Mat& undistorted_image);
+    void publisRawImage(const cv::Mat& warped_image);
+    void publishPreview(const cv::Mat& warped_image);
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr preview_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+
+    cv::VideoCapture camera_;
+    cv::Ptr<cv::aruco::Dictionary> aruco_markers_dict_ =
+        cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+
+    cv::Size frame_size_;
+    cv::Matx33f homography_matrix_{cv::Matx33f::eye()};
+    IntrinsicCameraParameters calibration_params_;
+};
+
+}  // namespace wbb
